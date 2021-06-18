a77104ede4523b88820c176db78e5d935e379201
rchry001 <84558191+rchry001@users.noreply.github.com>
2021-06-18 16:05:07 -0400

    adding movement to player icon

--- app/project_random.py
+++ app/project_random.py
@@ -16,19 +16,34 @@ white = (255,255,255)
 
 clock = pygame.time.Clock()
 crashed = False
-carImg = pygame.image.load('racecar.png')
+PlayerImg = pygame.image.load('racecar.png')
 
 def car(x,y):
-    gameDisplay.blit(carImg, (x,y))
+    gameDisplay.blit(PlayerImg, (x,y))
 
-x =  (display_width * 0.45)
+x =  (display_width * 0.4)
 y = (display_height * 0.8)
+x_change = 0
+car_speed = 1
+
 
 while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
-
+        #####################################
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
+        #####################################
+    ##################
+    x += x_change
+    ##################
     gameDisplay.fill(white)
     car(x,y)
 
